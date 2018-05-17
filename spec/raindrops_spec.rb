6e7fbdd8b6f2a9eb4af97ef0e18e5a135aded9e2
durranee <durranee@hotmail.com>
2018-05-17 12:11:07 +0100

    divisible_by? test passing and returning true as expected

--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -2,7 +2,7 @@ require './lib/raindrops'
 
 describe '#divisible_by' do
 
-  it 'returns true if number is divisible by self' do
+  it 'returns true if number is divisible by one being called on' do
     expect(10.divisible_by?(5)).to eq(true)
   end
 

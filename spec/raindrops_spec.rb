580e672f3e89c90514e2efef2ccc7b2d2625d127
durranee <durranee@hotmail.com>
2018-05-17 12:14:25 +0100

    raindrops() failing test for Pling

--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -1,7 +1,6 @@
 require './lib/raindrops'
 
 describe '#divisible_by' do
-
   it 'returns true if number is divisible by one being called on' do
     expect(10.divisible_by?(5)).to eq(true)
   end
@@ -9,4 +8,11 @@ describe '#divisible_by' do
   it 'returns false if number is not divisible by one being called on' do
     expect(11.divisible_by?(5)).to eq(false)
   end
+end
+
+describe '#raindrops' do
+  it 'returns Pling if factor includes 3' do
+    expect(raindrops(6)).to eq('Pling')
+  end
+
 end
